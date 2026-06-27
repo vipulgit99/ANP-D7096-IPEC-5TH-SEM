de309d93fbff48d096de0c3621c7bbfcf9df3541
Vipul <24ece031@ipec.org.in>
2026-06-27 09:14:19 +0530

    lab work

---
+++ lab_work/selection_statement_lab_work/average.py
@@ -0,0 +1,12 @@
+''' write a program to calculate average of five number '''
+# Program to calculate average of 5 numbers
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+num4 = float(input("Enter fourth number: "))
+num5 = float(input("Enter fifth number: "))
+
+average = (num1 + num2 + num3 + num4 + num5) / 5
+
+print("Average =", average)
---
+++ lab_work/selection_statement_lab_work/velocity.py
@@ -0,0 +1,9 @@
+''' write a program to calculate velocity when distance and time are provided '''
+# Program to calculate velocity
+
+distance = float(input("Enter distance: "))
+time = float(input("Enter time: "))
+
+velocity = distance / time
+
+print("Velocity =", velocity)
