49a8a3ae4afcd3e7fc368b2bc4c4b7b58afe2e22
Vipul <24ece031@ipec.org.in>
2026-06-27 10:11:26 +0530

    class work

---
+++ class_work/27th day_4 if else condition/parking Entry Validation.py
@@ -0,0 +1,16 @@
+# Parking Entry Validation Program
+
+# Take parking pass status as input
+parking_pass = int(input("Enter parking pass (1 = Valid Pass, 0 = No Pass): "))
+
+# Validate the input
+if parking_pass != 0 and parking_pass != 1:
+    print("Invalid Input")
+
+# Check if the parking pass is valid
+elif parking_pass == 1:
+    print("Entry Allowed")
+
+# If there is no valid parking pass
+else:
+    print("Entry Denied")
