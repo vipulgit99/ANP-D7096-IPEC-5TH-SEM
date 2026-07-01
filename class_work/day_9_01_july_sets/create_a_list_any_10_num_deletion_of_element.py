0c2951aa3e7931ff2f7b56e9d546e3a693f8f323
Vipul <24ece031@ipec.org.in>
2026-07-01 10:23:09 +0530

    create_a_list_any_10_num_deletion_of_element

--- class_work/day_9_01_july_sets/create_a_list_any_10_num_deletion_of_element.py
+++ class_work/day_9_01_july_sets/create_a_list_any_10_num_deletion_of_element.py
@@ -1,8 +1,8 @@
-
 # Program to create a list of 10 numbers and delete an element
-# from the position entered by the user
-'''----------------------coding----------------------'''
+# from the position entered by the user using pop()
 
+
+'''----------------------coding----------------------'''
 # Create an empty list
 num = []
 
@@ -14,34 +14,33 @@ for i in range(10):
 # Display the original list
 print("\nOriginal List:", num)
 
-# Ask the user for the position to delete
+# Ask the user to enter the position to delete
 position = int(input("Enter the position to delete (1-10): "))
 
-# Check whether the position is valid
+# Check if the position is valid
 if position >= 1 and position <= len(num):
-    # Delete the element
-    del num[position - 1]
+    deleted = num.pop(position - 1)   # Delete the element
 
-    # Display the updated list
+    print("Deleted Element:", deleted)
     print("Updated List:", num)
 else:
     print("Invalid Position!")
 
-
-
-'''---------------------output---------------------
-
-Enter a number: 1
+    '''------------------output------------------
+    Enter a number: 1
 Enter a number: 2
 Enter a number: 3
-Enter a number: 4
 Enter a number: 5
+Enter a number: 4
 Enter a number: 6
-Enter a number: 7
-Enter a number: 9
 Enter a number: 8
+Enter a number: 9
+Enter a number: 7
 Enter a number: 10
 
-Original List: [1, 2, 3, 4, 5, 6, 7, 9, 8, 10]
-Enter the position to delete (1-10): 4
-Updated List: [1, 2, 3, 5, 6, 7, 9, 8, 10]'''
+Original List: [1, 2, 3, 5, 4, 6, 8, 9, 7, 10]
+Enter the position to delete (1-10): 5
+Deleted Element: 4
+Updated List: [1, 2, 3, 5, 6, 8, 9, 7, 10]
+
+ '''
